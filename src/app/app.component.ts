d1740e858625856bd4883ebab370db3762fe7bf9
Quentin <you@example.com>
2024-12-05 15:07:09 +0100

    ajout icônes réseaux sociaux

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  standalone: true,
+  imports: [FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'refonte-ASCFR';
-}
+export class AppComponent {}
